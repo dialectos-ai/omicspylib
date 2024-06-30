035f57b0b67aef46eaa80483308aed2cc7ab0f5a
Kostas <kostas.tsolis@porphyrio.com>
2024-06-30 14:07:41 +0200

    checkpoint -> refactoring

--- omicspylib/datasets/peptides.py
+++ omicspylib/datasets/peptides.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
+
+import copy
 from typing import List, Optional
 
+import numpy as np
 import pandas as pd
 
 from omicspylib import ProteinsDataset
@@ -13,16 +16,56 @@ class PeptidesDatasetExpCondition(TabularExperimentalConditionDataset):
                  data: pd.DataFrame,
                  id_col: str,
                  experiment_cols: list,
-                 protein_id_col: Optional[str] = None) -> None:
+                 protein_id_col: Optional[str] = None,
+                 metadata: Optional[dict] = None) -> None:
         super().__init__(name=name, data=data, id_col=id_col, experiment_cols=experiment_cols)
         # todo - specify in the documentation that id_col should have values - and validate inputs
-        self._metadata = {
-            'peptide_to_protein': {}
-        }
-        if protein_id_col is not None:
-            records = data[[id_col, protein_id_col]].to_dict(orient='records')
-            for rec in records:
-                self._metadata['peptide_to_protein'][rec[id_col]] = rec[protein_id_col]
+        # todo - this in not a clean implementation of initializing the object and passing metadata - think of another solution
+        self._protein_id_col = protein_id_col
+        if metadata is None:
+            self._metadata = {
+                'peptide_to_protein': {}
+            }
+            if protein_id_col is not None:
+                records = data[[id_col, protein_id_col]].to_dict(orient='records')
+                for rec in records:
+                    self._metadata['peptide_to_protein'][rec[id_col]] = rec[protein_id_col]
+        else:
+            self._metadata = copy.deepcopy(metadata)
+
+    def filter(self,
+               min_frequency: Optional[int] = None,
+               na_threshold: float = 0.0) -> PeptidesDatasetExpCondition:
+        """
+        Filter dataset based on a given set of properties.
+
+        Parameters
+        ----------
+        min_frequency: int or None, optional
+            If specified, records of the dataset will be filtered based on their
+            within group frequency.
+        na_threshold: float or None, optional
+            Values below or equal to this threshold are considered missing.
+            Is used in to filter records based on the number of missing values.
+
+        Returns
+        -------
+        ProteinsDatasetExpCondition
+            A new instance of the dataset object, filtered based on the
+            user's input.
+        """
+        data = self._data.copy()
+        if min_frequency is not None:
+            valid_rows = np.sum(data > na_threshold, axis=1) >= min_frequency
+            data = data.loc[valid_rows, :].copy()
+
+        return PeptidesDatasetExpCondition(
+            name=self.name,
+            data=data.reset_index(),
+            id_col=self._id_col,
+            experiment_cols=data.columns.tolist(),
+            protein_id_col=self._protein_id_col,
+            metadata=self._metadata)
 
 
 class PeptidesDataset(TabularDataset):
