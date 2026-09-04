25f4726b1a49362d294ec55df22b7bc54038eb34
Kostas Tsolis <kostas.tsolia+alexion@alexion.com>
2026-09-04 13:41:52 +0300

    update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
